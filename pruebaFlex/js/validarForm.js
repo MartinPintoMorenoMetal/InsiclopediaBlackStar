e8e58f8fa7874b5d883738dc77905b9877188ef2
mauro_1995@live.com <mauro_1995@live.com>
2019-09-05 16:34:24 -0400

    FALTA

--- pruebaFlex/js/validarForm.js
+++ pruebaFlex/js/validarForm.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
                     email: true
                 },
 
-                c: {
+                apellidos: {
                     required: true,
                     apellidos: true
                 },
