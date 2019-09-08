23a1f61813cf8019403e591d3c06afada66428bc
mauro_1995@live.com <mauro_1995@live.com>
2019-09-08 13:54:06 -0400

    Cambios Formularios

--- pruebaFlex/js/validarForm.js
+++ pruebaFlex/js/validarForm.js
@@ -1,160 +1,114 @@
 //jquery
-$(document).ready(function() {
-    $('#fecha').val(new Date().toDateInputValue());
-    document.getElementById('fecha').defaultValue = new Date().toDateInputValue();
-    $(function() {
-        $("contacto").validate({
-            rules: {
-                nombre: {
-                    required: true,
-                    nombre: true
-                },
-
-                email: {
-                    required: true,
-                    email: true
-                },
-
-                apellidos: {
-                    required: true,
-                    apellidos: true
-                },
-
-                usuario: {
-                    required: true,
-                    usuario: true
-                },
+$(function() {
+    ("#contacto").validate({
+        rules: {
+            email: {
+                required: true,
+                email: true
             },
-            messages: {
-                nombre: {
-                    required: 'Ingresa tu nombre ',
-                    minlength: 'Largo insuficiente'
-                },
-                email: {
-                    required: 'Ingresa un correo electronico',
-                    email: 'Formato de correo no válido'
-                },
-                apellidos: {
-                    required: 'Ingresa tus apellidos ',
-                    minlength: 'Largo insuficiente'
-                },
-                usuario: {
-                    required: 'Ingresa nombre de usuario',
-                    minlength: 'Largo insuficiente'
-                },
-            }
-        });
+
+            nombre: {
+                required: true,
+                nombre: true
+            },
+
+            apellidos: {
+                required: true,
+                apellidos: true
+
+            },
+            usuario: {
+                required: true,
+                usuario: true
+
+            },
+
+            contraseña: {
+                required: true,
+                usuario: true
+
+            },
+
+        },
+        messages: {
+            email: {
+                required: 'Ingresa tu correo electrónico',
+                email: 'Formato de correo no válido'
+            },
+            nombre: {
+                required: 'Ingresa tu nombre',
+                minlength: 'Largo insuficiente'
+            },
+            apellidos: {
+                required: 'Ingresa tu apellidos',
+                minlength: 'Largo insuficiente'
+
+            },
+            usuario: {
+                required: 'Ingresa tu usuario',
+                minlength: 'Largo insuficiente'
+
+            },
+            contraseña: {
+                required: 'Ingresa tu contraseña',
+                minlength: 'Largo insuficiente'
+
+            },
+        }
     });
+});
 
+$(document).ready(function() {
+    $("#contact-form").validate({
+        event: "blur",
+        rules: { 'nombre': "required", 'email': "required email", 'apellidos': "required", 'usuario': "required", 'contraseña': "required" },
+        messages: { 'nombre': "Por favor, indica tu nombre", 'email': "Por favor, indica tu correo", 'apellidos': "Por favor, indica tus apellidos", 'usuario': "Por favor, indica tu nombre de usuario", 'contraseña': "Por favor, indica tu contraseña de usuario" },
+        debug: true,
+        errorElemen: "label",
+        submitHandler: function(form) {
+            $("#alert").show();
+            $("#alert").html("<img src='images/ajax-loader.gif' style='vertical-align:middle;margin:0 10px 0 0' /><strong>Enviando mensaje...</strong>");
+            setTimeout(function() {
+                $('#alert').fadeOut('slow');
+            }, 5000);
+            $.ajax({
+                type: "POST",
+                url: "#",
+                data: "nombre=" + escape($('#nombre').val()) + "&email=" + escape($('#email').val()) + "&apellidos=" + escape($('#apellidos').val()) + "&usuario=" + escape($('#usuario').val()),
+                success: function(msg) {
+                    $("#alert").html(msg);
+                    document.getElementById("nombre").value = "";
+                    document.getElementById("email").value = "";
+                    document.getElementById("apellidos").value = "";
+                    document.getElementById("usuario").value = "";
+                    document.getElementById("contraseña").value = "";
+                    setTimeout(function() {
+                        $('#alert').fadeOut('slow');
+                    }, 5000);
 
-    $(function() {
-        $('.btn-enviar').on('click', function() {
-
-            var nombre = $('nombre').val();
-            if (nombre == null || nombre.length == 0) {
-                alert('ERROR: El campo nombre no debe ir vacío ' + nombre);
-                $('#nombre').addClass('error');
-                $('#nombre').removeClass('exito');
-            } else {
-                $('#nombre').removeClass('error');
-                $('#nombre').addClass('exito');
-            }
-
-            var email = $('email').val();
-            if (email == null || email.length == 0) {
-                alert('ERROR: El campo email no debe ir vacío ' + email);
-                $('#email').addClass('error');
-                $('#email').removeClass('exito');
-            } else {
-                $('#email').removeClass('error');
-                $('#email').addClass('exito');
-            }
-
-            var apellidos = $('apellidos').val();
-            if (apellidos == null || apellidos.length == 0) {
-                alert('ERROR: El campo apellidos no debe ir vacío ' + apellidos);
-                $('#apellidos').addClass('error');
-                $('#apellidos').removeClass('exito');
-            } else {
-                $('#apellidos').removeClass('error');
-                $('#apellidos').addClass('exito');
-            }
-
-            var usuario = $('usuario').val();
-            if (usuario == null || usuario.length == 0) {
-                alert('ERROR: El campo usuario no debe ir vacío ' + usuario);
-                $('#usuario').addClass('error');
-                $('#usuario').removeClass('exito');
-            } else {
-                $('#usuario').removeClass('error');
-                $('#usuario').addClass('exito');
-            }
-        })
+                }
+            });
+        }
     });
-
-
-    // funciones de validaciones para el nombre
-    function validarNombre() {
-        var nombre = document.getElementById("nombre").value;
-        var txtNombre = document.getElementById("nombre");
-        // Texto obligatorio
-        if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
-            alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
-            txtNombre.classList.add('error');
-        } else {
-            txtNombre.classList.remove('error');
-        }
-    }
-
-    // funciones de validaciones para el email
-    function validarCorreo() {
-
-        var email = document.getElementById("email").value;
-        var txtEmail = document.getElementById("email");
-
-        //Test correo
-        if (email == null || email.length == 0 || !(/\S+@\S+\.\S+/.test(email))) {
-            alert('ERROR: Debe escribir un email válido');
-            txtEmail.classList.add('error');
-        } else {
-            txtEmail.classList.remove('error');
-        }
-    }
-
-    // funciones de validaciones para el apellido
-    function validarApellido() {
-        var apellidos = document.getElementById("apellidos").value;
-        var txtApellidos = document.getElementById("apellidos");
-
-        // Texto obligatorio
-        if (apellidos == null || apellidos.length == 0 || /^\s+$/.test(apellidos)) {
-            alert('ERROR: El campo Apellido no debe ir vacío o lleno de solamente espacios en blanco');
-            txtApellidos.classList.add('error');
-        } else {
-            txtApellidos.classList.remove('error');
-        }
-    }
-
-    // funciones de validaciones para el usuario
-    function validarUsuario() {
-        var usuario = document.getElementById("usuario").value;
-        var txtUsuario = document.getElementById("usuario");
-
-        // Texto obligatorio
-        if (usuario == null || usuario.length == 0 || /^\s+$/.test(usuario)) {
-            alert('ERROR: El campo usuario no debe ir vacío o lleno de solamente espacios en blanco');
-            txtUsuario.classList.add('error');
-        } else {
-            txtUsuario.classList.remove('error');
-        }
-    }
-
 });
 
 
+function validarFormulario() {
+    jQuery.validator.messages.required = 'Esta campo es obligatorio.';
+    jQuery.validator.messages.number = 'Esta campo debe ser numérico.';
+    jQuery.validator.messages.email = 'El de correo es incorrecto.';
+    jQuery.validator.messages.contraseña = 'La contraseña es incorrecto.';
+    $("#enviar").click(function() {
+        var validado = $("#contacto").valid();
+        if (validado) {
+            alert('El formulario es correcto.');
+        }
+    });
+}
+
 
 $(function() {
-    $('#nombre').on('blur', function() {
+    $('.boton').on('click', function() {
         var nombre = $('#nombre').val();
 
         if (nombre == null || nombre.length == 0) {
@@ -165,12 +119,8 @@ $(function() {
             $('#nombre').removeClass('error');
             $('#nombre').addClass('exito');
         }
-    })
-});
 
-$(function() {
-    $('#email').on('blur', function() {
-        var email = $('#email').val();
+        var email = $('#mail').val();
 
         if (email == null || email.length == 0) {
             alert('ERROR: El campo email no debe ir vacío' + email);
@@ -180,11 +130,7 @@ $(function() {
             $('#email').removeClass('error');
             $('#email').addClass('exito');
         }
-    })
-});
 
-$(function() {
-    $('#apellidos').on('blur', function() {
         var apellidos = $('#apellidos').val();
 
         if (apellidos == null || apellidos.length == 0) {
@@ -195,12 +141,7 @@ $(function() {
             $('#apellidos').removeClass('error');
             $('#apellidos').addClass('exito');
         }
-    })
-});
 
-
-$(function() {
-    $('#usuario').on('blur', function() {
         var usuario = $('#usuario').val();
 
         if (usuario == null || usuario.length == 0) {
@@ -211,12 +152,98 @@ $(function() {
             $('#usuario').removeClass('error');
             $('#usuario').addClass('exito');
         }
+
+        var contraseña = $('#contraseña').val();
+
+        if (contraseña == null || contraseña.length == 0) {
+            alert('ERROR: El campo contraseña no debe ir vacío' + contraseña);
+            $('#contraseña').addClass('error');
+            $('#contraseña').removeClass('exito');
+        } else {
+            $('#contraseña').removeClass('error');
+            $('#contraseña').addClass('exito');
+        }
+
     })
 });
 
+javaScript
+
+function validarNombre() {
+
+    var nombre = document.getElementById("nombre").value;
+    var txtNombre = document.getElementById("nombre");
+
+    //Test campo obligatorio
+    if (nombre == null || nombre.length == 0 || /^\s+$/.test(nombre)) {
+        alert('ERROR: El campo nombre no debe ir vacío o lleno de solamente espacios en blanco');
+        txtNombre.classList.add('error');
+    } else {
+        txtNombre.classList.remove('error');
+    }
+}
+
+function validarCorreo() {
+
+    var correo = document.getElementById("email").value;
+    var txtCorreo = document.getElementById("email");
+
+    //Test correo
+    if (correo == null || correo.length == 0 || !(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/.test(correo))) {
+        alert('ERROR: Debe escribir un correo válido');
+        txtCorreo.classList.add('error');
+    } else {
+        txtCorreo.classList.remove('error');
+    }
+}
+
+function validarApellidos() {
+
+    var apellidos = document.getElementById("apellidos").value;
+    var txtApellidos = document.getElementById("apellidos");
+
+    //Test campo obligatorio
+    if (apellidos == null || apellidos.length == 0 || /^\s+$/.test(apellidos)) {
+        alert('ERROR: El campo apellidos no debe ir vacío o lleno de solamente espacios en blanco');
+        txtApellidos.classList.add('error');
+    } else {
+        txtApellidos.classList.remove('error');
+    }
+}
+
+function validarUsuario() {
+
+    var usuario = document.getElementById("usuario").value;
+    var txtUsuario = document.getElementById("usuario");
+
+    //Test campo obligatorio
+    if (usuario == null || usuario.length == 0 || /^\s+$/.test(usuario)) {
+        alert('ERROR: El campo usuario no debe ir vacío o lleno de solamente espacios en blanco');
+        txtUsuario.classList.add('error');
+    } else {
+        txtUsuario.classList.remove('error');
+    }
+}
+
+function validarContraseña() {
+
+    var contraseña = document.getElementById("contraseña").value;
+    var pswContraseña = document.getElementById("contraseña");
+
+    //Test campo obligatorio
+    if (contraseña == null || contraseña.length == 0 || /^\s+$/.test(contraseña)) {
+        alert('ERROR: El campo contraseña no debe ir vacío o lleno de solamente espacios en blanco');
+        pswContraseña.classList.add('error');
+    } else {
+        pswContraseña.classList.remove('error');
+    }
+}
+
+
 function validarFormulario() {
-    validarNombre()
-    validarApellido()
-    validarCorreo()
-    validarUsuario()
-};
+    validarNombre();
+    validarCorreo();
+    validarApellidos();
+    validarUsuario();
+    validarContraseña();
+}
