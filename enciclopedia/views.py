dd2dc15c6c3b44851be61bd744862ce6bd3c15e2
IsaiasDiaz777 <48770029+IsaiasDiaz777@users.noreply.github.com>
2019-10-24 19:13:18 -0300

    Los demas HTML

--- enciclopedia/views.py
+++ enciclopedia/views.py
@@ -1,6 +1,30 @@
 from django.shortcuts import render
-from django.utils import timezone
 
 # Create your views here.
+def index(request):
+    return render(request, 'enciclopedia/index.html')
+
 def criaturas(request):
     return render(request, 'enciclopedia/criaturas.html')
+
+def formulario(request):
+    return render(request, 'enciclopedia/formulario.html')
+
+def sesion(request):
+    return render(request, 'enciclopedia/sesion.html')
+
+def galeria(request):
+    return render(request, 'enciclopedia/galeria.html')
+
+def perfilA(request):
+    return render(request, 'enciclopedia/perfilA.html')
+
+def perfilB(request):
+    return render(request, 'enciclopedia/perfilB.html')
+
+def perfilC(request):
+    return render(request, 'enciclopedia/perfilC.html')
+
+def perfilD(request):
+    return render(request, 'enciclopedia/perfilD.html')
+
