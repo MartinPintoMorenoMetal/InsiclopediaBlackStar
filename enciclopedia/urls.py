86f3683b3ffdb00983d551754a4bbd676ba1c21a
mauro_1995@live.com <mauro_1995@live.com>
2019-10-28 00:39:59 -0300

    import and path URLS

--- enciclopedia/urls.py
+++ enciclopedia/urls.py
@@ -1,17 +1,26 @@
+from django.contrib import admin
+from django.urls import path.include
+from django.contrib.auth.decorators import login_required
+from django.enciclopedia.views import Inicio
+
 from django.urls import path
-from django.contrib.auth,views import login, logout_then_login
 from . import views
  
 urlpatterns = [
-    
+
+
     path('', views.index, name='index'),
     path('index', views.index, name='index'),
     path('criaturas', views.criaturas, name='criaturas'),
     path('formulario', views.formulario, name='formulario'),
     path('galeria', views.galeria, name='galeria'),
-    path('sesion', views.sesion, name='sesion'),
+    path('login', views.sesion, name='login'),
     path('perfilA', views.perfilA, name='perfilA'),
     path('perfilB', views.perfilB, name='perfilB'),
     path('perfilC', views.perfilC, name='perfilC'),
     path('perfilD', views.perfilD, name='perfilD'),
+    
+    
+    path('enciclopedia/',include(('apps.enciclopedia.urls','enciclopedia'))),
+    path('',login_required(Inicio.as_view()), name = 'index'),
 ]
