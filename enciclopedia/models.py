09633c891918015b27b2a998644c993ac7ffc077
IsaiasDiaz777 <48770029+IsaiasDiaz777@users.noreply.github.com>
2019-11-10 22:16:20 -0300

    models

--- enciclopedia/models.py
+++ enciclopedia/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.utils.translation import ugettext as _
  
  
 class Post(models.Model):
@@ -11,9 +12,15 @@ class Post(models.Model):
     published_date = models.DateTimeField(
             blank=True, null=True)
  
-def publish(self):
-    self.published_date = timezone.now()
-    self.save()
- 
-def __str__(self):
-    return self.title
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+    
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        permissions = (
+            ('administrador',_('Es administrador')),
+            ('cliente',_('Es cliente')),
+        )
