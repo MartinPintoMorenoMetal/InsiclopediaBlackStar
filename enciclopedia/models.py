1eec42740e23dbf984a2653b113911c75bfd2d01
MartinPintoMorenoMetal <martinpintomoren@hotmail.com>
2019-11-30 15:24:37 -0300

    cosas

--- enciclopedia/models.py
+++ enciclopedia/models.py
@@ -3,25 +3,26 @@ from django.utils import timezone
 from django.utils.translation import ugettext as _
 
 
+
 class Post(models.Model):
-    
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     Descripcion = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
+    nombreCriatura = models.CharField(max_length=200)
+   
 
     def publish(self):
         self.published_date = timezone.now()
         self.save()
-    
+
     def __str__(self):
         return self.title
-    
+
     class Meta:
         permissions = (
-            ('administrador',_('Es administrador')),
-            ('cliente',_('Es cliente')),
+            ('administrador', _('Es administrador')),
+            ('cliente', _('Es cliente')),
         )
-
 
 
