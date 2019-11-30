7baa4ce8c25492a5af6068f4ca706578f7ebadc7
MartinPintoMorenoMetal <martinpintomoren@hotmail.com>
2019-11-30 16:17:13 -0300

    cosas

--- enciclopedia/admin.py
+++ enciclopedia/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post,Selection
  
 
 admin.site.register(Post)
+admin.site.register(Selection)
+
 
--- enciclopedia/models.py
+++ enciclopedia/models.py
@@ -2,16 +2,20 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext as _
 
+#Criaturas
+class Selection(models.Model):
+    name = models.CharField(max_length=40)
 
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     url = models.CharField(max_length=200)
     Descripcion = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    nombreCriatura = models.CharField(max_length=200)
+    Selection = models.ForeignKey(Selection, on_delete=models.CASCADE)
    
-
     def publish(self):
         self.published_date = timezone.now()
         self.save()
@@ -24,5 +28,6 @@ class Post(models.Model):
             ('administrador', _('Es administrador')),
             ('cliente', _('Es cliente')),
         )
+
 
 
